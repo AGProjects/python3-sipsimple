bc29f63a3dc346caf07f10c236a7e8d73e1b4f8c
Adrian Georgescu <ag@ag-projects.com>
2021-02-10 15:10:07 +0000

    Fixed DNS lookup

--- sipsimple/lookup.py
+++ sipsimple/lookup.py
@@ -256,9 +256,9 @@ class DNSLookup(object):
         try:
             # If the host part of the URI is an IP address, we will not do any lookup
             if re.match("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", uri.host.decode()):
-                transport = 'tls' if uri.secure else uri.transport.decode().lower()
+                transport = 'tls' if uri.secure else uri.transport.lower()
                 if transport not in supported_transports:
-                    raise DNSLookupError("Transport %s dictated by URI is not supported" % transport)
+                    raise DNSLookupError("IP transport %s dictated by URI is not supported" % transport)
                 port = uri.port or (5061 if transport=='tls' else 5060)
                 route = [Route(address=uri.host, port=port, transport=transport)]
                 return route
@@ -272,7 +272,7 @@ class DNSLookup(object):
             if uri.port:
                 transport = 'tls' if uri.secure else uri.transport.decode().lower()
                 if transport not in supported_transports:
-                    raise DNSLookupError("Transport %s dictated by URI is not supported" % transport)
+                    raise DNSLookupError("Host transport %s dictated by URI is not supported" % transport)
                 addresses = self._lookup_a_records(resolver, [uri.host.decode()], log_context=log_context)
                 if addresses[uri.host.decode()]:
                     return [Route(address=addr, port=uri.port, transport=transport, tls_name=uri.host) for addr in addresses[uri.host.decode()]]
