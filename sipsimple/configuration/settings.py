25112ec746b659f3256d4a7e878406de0a3f3a60
Adrian Georgescu <ag@ag-projects.com>
2021-06-14 12:24:56 +0000

    Added dependencies to enable AMR audio codecs

--- sipsimple/configuration/settings.py
+++ sipsimple/configuration/settings.py
@@ -68,7 +68,7 @@ class LogsSettings(SettingsGroup):
 class RTPSettings(SettingsGroup):
     port_range = Setting(type=PortRange, default=PortRange(50000, 50500))
     timeout = Setting(type=NonNegativeInteger, default=30)
-    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA', 'speex', 'iLBC', 'GSM', 'AMR-WB')))
+    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA', 'speex', 'iLBC', 'GSM')))
     video_codec_list = Setting(type=VideoCodecList, default=VideoCodecList(('H264', 'VP8', 'VP9')))
 
 
