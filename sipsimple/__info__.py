5fc983cc057eb954cb3e70b256bf6b6278406cee
Adrian Georgescu <ag@ag-projects.com>
2021-06-26 18:17:13 +0000

    Added version 5.2.2

--- sipsimple/__info__.py
+++ sipsimple/__info__.py
@@ -5,7 +5,7 @@ __project__ = "python3-sipsimple"
 __summary__ = "SIP SIMPLE implementation for Python"
 __webpage__ = "https://sipsimpleclient.org"
 
-__version__ = "5.2.1"
+__version__ = "5.2.2"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
