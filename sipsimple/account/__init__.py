dc5a8fb3d9e84173d91273f4a383cf70b05f29a0
Tijmen de Mes <tijmen@ag-projects.com>
2023-02-15 16:36:32 +0000

    Run account delete inside green thread

--- sipsimple/account/__init__.py
+++ sipsimple/account/__init__.py
@@ -211,6 +211,7 @@ class Account(SettingsObject):
         notification_center.remove_observer(self, sender=self._self_presence_subscriber)
         notification_center.remove_observer(self, sender=self._dialog_subscriber)
 
+    @run_in_green_thread
     def delete(self):
         if self._deleted:
             return
