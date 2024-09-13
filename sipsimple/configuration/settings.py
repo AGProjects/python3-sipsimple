a0f5dc7c13ff9f1944db11c7337eb35cabe60779
Tijmen de Mes <tijmen@ag-projects.com>
2024-09-13 12:06:01 +0000

    Removed no longer supported codecs

--- sipsimple/configuration/settings.py
+++ sipsimple/configuration/settings.py
@@ -70,7 +70,7 @@ except ImportError:
     class RTPSettings(SettingsGroup):
         port_range = Setting(type=PortRange, default=PortRange(50000, 50500))
         timeout = Setting(type=NonNegativeInteger, default=30)
-        audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA', 'speex', 'iLBC', 'GSM')))
+        audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA')))
         video_codec_list = Setting(type=VideoCodecList, default=VideoCodecList(('H264', 'VP8', 'VP9')))
 
 
